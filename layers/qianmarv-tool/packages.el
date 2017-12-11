e462dee213bd5fa7860155ec9740d5c0960578e7
qianmarv <qianmarv@gmail.com>
2017-12-11 13:39:09 +0800

    add tools setting

---
+++ layers/qianmarv-tool/packages.el
@@ -0,0 +1,29 @@
+;;; packages.el --- qianmarv-tool layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Marvin Qian <qianmarv@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+
+(defconst qianmarv-tool-packages
+  '(
+    google-translate
+    ))
+
+;;------------------------------------------------------------------------------
+;; google-translate
+;; https://github.com/atykhonov/google-translate
+;;------------------------------------------------------------------------------
+(defun qianmarv-coding/init-google-translate()
+  (use-package google-translate)
+  :config
+  (progn
+    (setq google-translate-default-target-language "zh-CN")
+    )
+  )
+

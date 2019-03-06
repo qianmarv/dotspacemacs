2e6a27eb2685bb47e07abc6f773da9d880ad01dd
Marvin Qian <qianmarv@gmail.com>
2019-03-06 13:10:31 +0800

    Fix

--- layers/qianmarv-org/packages.el
+++ layers/qianmarv-org/packages.el
@@ -164,10 +164,10 @@
       ;; Setup for GTD
       ;; Refer to http://www.i3s.unice.fr/~malapert/org/tips/emacs_orgmode.html
       (setq org-todo-keywords
-            '(
-              (sequence "TODO(t)" "IDEA(i)" "STARTED(s)" "NEXT(n)" "WAITING(w)" "|" "DONE(d)")
-              (sequence "|" "CANCELED(c)" "DELEGATED(l)" "SOMEDAY(f)")
-              ))
+            (quote ((sequence "IDEA(i)" "TODO(t)" "NEXT(n)" "|" "DONE(d!/!)")
+                    (sequence "PROJECT(p)" "|" "DONE(d!/!)" "CANCELLED(c@/!)")
+                    (sequence "WAITING(w@/!)" "DELEGATED(e!)" "HOLD(h)" "|" "CANCELLED(c@/!)")))
+            org-todo-repeat-to-state "TODO")
 
       (setq org-todo-keyword-faces
             '(("IDEA" . (:foreground "GoldenRod" :weight bold))
@@ -211,6 +211,45 @@
               ;; ("BONUS" . (:foreground "GoldenRod" :weight bold))
               )
             )
+
+
+;;; Refiling
+
+      (setq org-refile-use-cache nil)
+
+      ;; Targets include this file and any file contributing to the agenda - up to 5 levels deep
+      (setq org-refile-targets '((nil :maxlevel . 5) (org-agenda-files :maxlevel . 5)))
+
+      (add-to-list 'org-agenda-after-show-hook 'org-show-entry)
+
+      ;; (advice-add 'org-refile :after (lambda (&rest _) (org-save-all-org-buffers)))
+
+      ;; Exclude DONE state tasks from refile targets
+      (defun sanityinc/verify-refile-target ()
+        "Exclude todo keywords with a done state from refile targets."
+        (not (member (nth 2 (org-heading-components)) org-done-keywords)))
+      (setq org-refile-target-verify-function 'sanityinc/verify-refile-target)
+
+      (defun sanityinc/org-refile-anywhere (&optional goto default-buffer rfloc msg)
+        "A version of `org-refile' which allows refiling to any subtree."
+        (interactive "P")
+        (let ((org-refile-target-verify-function))
+          (org-refile goto default-buffer rfloc msg)))
+
+      (defun sanityinc/org-agenda-refile-anywhere (&optional goto rfloc no-update)
+        "A version of `org-agenda-refile' which allows refiling to any subtree."
+        (interactive "P")
+        (let ((org-refile-target-verify-function))
+          (org-agenda-refile goto rfloc no-update)))
+
+      ;; Targets start with the file name - allows creating level 1 tasks
+      ;;(setq org-refile-use-outline-path (quote file))
+      (setq org-refile-use-outline-path t)
+      (setq org-outline-path-complete-in-steps nil)
+
+      ;; Allow refile to create parent tasks with confirmation
+      (setq org-refile-allow-creating-parent-nodes 'confirm)
+
 
       (setq org-enable-priority-commands nil)
       ;; --------------------------------------------------------------------
@@ -267,14 +306,7 @@ TODO create the notebook if NOT exist."
           ("rw" "Weekly Review"  entry
            (file+olp+datetree ,journal-book) (file ,(qianmarv/expand-template "weekly_review")) :tree-type week :time-prompt t)
           ("rm" "Monthly Review"  entry
-           (file+olp+datetree ,journal-book) (file ,(qianmarv/expand-template "monthly_review")) :tree-type week :time-prompt t)))
-  ;; (setq org-agenda-files (directory-files qianmarv/gtd-directory "\.org$"))
-  ;; (setq org-agenda-files `(,journal-book
-  ;;                          ,inbox-book
-  ;;                          ,capture-book
-  ;;                          ,agenda-book
-  ;;                          ,project-book))
-  )      
+           (file+olp+datetree ,journal-book) (file ,(qianmarv/expand-template "monthly_review")) :tree-type week :time-prompt t))))
 
 ;;; Show the clocked-in task - if any - in the header line
 (defun qianmarv/show-org-clock-in-header-line ()
@@ -292,39 +324,9 @@ TODO create the notebook if NOT exist."
             (define-key org-clock-mode-line-map [header-line mouse-1] 'org-clock-menu)
             ;; )
 
-      ;;
-      ;;FIXME If the Emacs keep open then the file name would be not correct if passes one month!
-      ;;TODO Put it in a single Journal.org file, and like other files, keep data for one year only.
-      ;; (setq qianmarv-org/gtd-path "~/Org/GTD")
-      ;; (setq qianmarv-org/journal-file (qianmarv-org/get-monthly))
-      ;; (setq org-capture-templates
-      ;;       `(("c" "Collect/Capture")
-      ;;         ("ci" "Capture Ideas/Mighty new todos" entry (file+headline ,(format "%s/Inbox.org" qianmarv-org/gtd-path ) "Ideas") "* IDEA %?\n %i\n")
-      ;;         ("cb" "Books want Read" entry (file+headline ,(format "%s/Inbox.org" qianmarv-org/gtd-path ) "Books") (file "~/Emacs/GTD/templates/book.tpl"))
-      ;;         ("cm" "Moviews want Watch" entry (file+headline ,(format "%s/Inbox.org" qianmarv-org/gtd-path ) "Movie") "* IDEA %?\n %i\n")
-      ;;         ("cj" "Journals, Morning Write" entry (file+function qianmarv-org/journal-file qianmarv-org/find-date-entry)
-      ;;          "* Morning Write \n\t%U\n\t%?")
-      ;;         ("cn" "Take Notes" item (file+function qianmarv-org/journal-file qianmarv-org/find-date-entry-notes) "Note taken on %U \\\\\n%?\n%K " :empty-lines 1)
-      ;;         ("b" "Break / Unplanned Interrupt")
-      ;;         ("bi" "Other Interrupt - Requires Refile" entry (file+headline ,(format "%s/Event.org" qianmarv-org/gtd-path ) "Other Interrupts") "* DONE %? \n%U %i\n" :clock-in t :clock-resume t)
-      ;;         ("bp" "Phone Calls" entry (file+headline ,(format "%s/Event.org" qianmarv-org/gtd-path ) "Phone Calls") "* DONE %? \n%U %i\n" :clock-in t :clock-resume t)
-      ;;         ("bs" "Someone Inquiry" entry (file+headline ,(format "%s/Event.org" qianmarv-org/gtd-path ) "Someone Inquiry") "* DONE %? \n%U %i\n" :clock-in t :clock-resume t)
-      ;;         ("bb" "Breaks, Tea Break / WC / etc..." entry (file+headline ,(format "%s/Event.org" qianmarv-org/gtd-path ) "Short Break") "* DONE %? \n%U %i\n" :clock-in t :clock-resume t)
-      ;;         ("r" "Review")
-      ;;         ("rd" "Daily Review"  entry (file+function qianmarv-org/journal-file qianmarv-org/find-date-entry) (file "~/Emacs/GTD/templates/daily_review.tpl"))
-      ;;         ("rw" "Weekly Review" entry (file+function qianmarv-org/journal-file qianmarv-org/find-week-entry) (file "~/Emacs/GTD/templates/weekly_review.tpl"))
-      ;;         ("rm" "Month Review" entry (file+function qianmarv-org/journal-file qianmarv-org/find-date-entry) "* Monthly Review\n%U\n%?")
-      ;;         ))
 
       ;; (setq org-agenda-files  `(,qianmarv-org/gtd-path))
       (setq org-agenda-compact-blocks nil)
-            ;; `(
-            ;;   ,(format "%s/Projects.org" qianmarv-org/gtd-path)
-            ;;   ,(format "%s/Event.org" qianmarv-org/gtd-path)
-            ;;   ,(format "%s/Agenda.org" qianmarv-org/gtd-path)
-            ;;   ,(format "%s/Inbox.org" qianmarv-org/gtd-path)
-            ;;   ,(format "%s/Habit.org" qianmarv-org/gtd-path)
-            ;;   ))
       ;;      (spacemacs/set-leader-keys "'" 'qianmarv-org/insert-screenshot)
       ;; Re-align tags when window shape changes
       (add-hook 'org-agenda-mode-hook
@@ -333,25 +335,41 @@ TODO create the notebook if NOT exist."
       ;; Settings for Reminder - appt
       ;; Refer to: https://emacs.stackexchange.com/questions/3844/good-methods-for-setting-up-alarms-audio-visual-triggered-by-org-mode-events
       ;; --------------------------------------------------------------------
-      (setq appt-disp-window-function 'qianmarv-org/show-alarm)
-      (setq appt-message-warning-time 5) ; Show notification 5 minutes before event
-      (setq appt-display-interval appt-message-warning-time) ; Disable multiple reminders
-      (setq appt-display-mode-line nil)
-      ;; Add Desktop Notification
-      ;; Refer to https://gist.github.com/jstewart/7664823
-      (add-hook 'org-pomodoro-finished-hook
-                (lambda ()
-                  (qianmarv-org/show-alarm 0 0 "Pomodoro completed! - Time for a break.")))
 
-      (add-hook 'org-pomodoro-break-finished-hook
-                (lambda ()
-                  (qianmarv-org/show-alarm 0 0 "Pomodoro Short Break Finished - Ready for Another?")))
+      ;; Different Apps to Be Called Under Different OS Platform
+      ;;   Win: Powershell Tool https://github.com/Windos/BurntToast
+      (defun qianmarv-org/show-alarm (min-to-app new-time message)
+        (cond ((string-equal system-type "windows-nt")
+               (call-process "powershell"
+                             nil
+                             t
+                             nil
+                             (format " New-BurntToastNotification -Text '%s' -Sound 'Alarm2' -SnoozeAndDismiss" message)
+                             ))))
 
-      (add-hook 'org-pomodoro-long-break-finished-hook
-                (lambda ()
-                  (qianmarv-org/show-alarm 0 0 "Pomodoro Long Break Finished - Ready for Another?")))
+      (setq org-show-notification-handler
+            (lambda (msg) (qianmarv-org/show-alarm nil nil msg)))
 
-      (add-hook 'org-pomodoro-killed-hook
-                (lambda ()
-                  (qianmarv-org/show-alarm 0 0 "Pomodoro Killed - One does not simply kill a pomodoro!")))
+
+      ;; (setq appt-disp-window-function 'qianmarv-org/show-alarm)
+      ;; (setq appt-message-warning-time 5) ; Show notification 5 minutes before event
+      ;; (setq appt-display-interval appt-message-warning-time) ; Disable multiple reminders
+      ;; (setq appt-display-mode-line nil)
+      ;; ;; Add Desktop Notification
+      ;; ;; Refer to https://gist.github.com/jstewart/7664823
+      ;; (add-hook 'org-pomodoro-finished-hook
+      ;;           (lambda ()
+      ;;             (qianmarv-org/show-alarm 0 0 "Pomodoro completed! - Time for a break.")))
+
+      ;; (add-hook 'org-pomodoro-break-finished-hook
+k     ;;           (lambda ()
+      ;;             (qianmarv-org/show-alarm 0 0 "Pomodoro Short Break Finished - Ready for Another?")))
+
+      ;; (add-hook 'org-pomodoro-long-break-finished-hook
+      ;;           (lambda ()
+      ;;             (qianmarv-org/show-alarm 0 0 "Pomodoro Long Break Finished - Ready for Another?")))
+
+      ;; (add-hook 'org-pomodoro-killed-hook
+      ;;           (lambda ()
+      ;;             (qianmarv-org/show-alarm 0 0 "Pomodoro Killed - One does not simply kill a pomodoro!")))
       )))
